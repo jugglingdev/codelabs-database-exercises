1e5cf5bfe10bae38ab7e085a67a5d5279f1dd69f
jugglingdev <kaylamarie1785@gmail.com>
2024-01-25 18:37:12 -0600

    Complete Exercise 1

---
+++ exercise-1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Customers (
+    CustomerID int,
+    Name varchar(255),
+    Email varchar(255)
+);
+
+INSERT INTO Customers
+VALUES (1, 'Alice Johnson', 'alice.johnson@email.com'),
+       (2, 'Bob Smith', 'bob.smith@email.com'),
+       (3, 'Charlie Brown', 'charlie.brown@email.com');
+       
